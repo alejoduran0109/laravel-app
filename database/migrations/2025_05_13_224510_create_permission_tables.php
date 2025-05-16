765d1fb008a6a75de24e6955c09ae4a0892f75ec
Ordnard <alejoduran0109@hotmail.com>
2025-05-15 23:37:14 -0500

    Ajustes

--- database/migrations/2025_05_13_224510_create_permission_tables.php
+++ database/migrations/2025_05_13_224510_create_permission_tables.php
@@ -28,6 +28,7 @@ return new class extends Migration
             // $table->engine('InnoDB');
             $table->bigIncrements('id'); // permission id
             $table->string('name');       // For MyISAM use string('name', 225); // (or 166 for InnoDB with Redundant/Compact row format)
+            $table->string('title')->nullable(); // Remove ->after('name')
             $table->string('guard_name'); // For MyISAM use string('guard_name', 25);
             $table->timestamps();
 
